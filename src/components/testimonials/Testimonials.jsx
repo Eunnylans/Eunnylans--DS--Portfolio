9d374528bf10865627c41feae4e16296fa0fddd5
Eunice Abieyuwa Igbinedion <eunice.igbinedion@yahoo.com>
2024-07-23 09:43:53 +0200

    Update

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaLinkedinIn } from "react-icons/fa";
 import { testimonials } from "../../Data";
 
-import Icon from "../../assets/lol.png";
+import Icon from "../../assets/quo.png";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -11,11 +11,12 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 // import required modules
-import { Pagination } from 'swiper/modules';
+import { Pagination } from "swiper/modules";
 
 import "./testimonials.css";
 
 const Testimonials = () => {
+
   return (
     <section className="testimonials section" id="testimonial">
       <h2 className="section__title text-cs">Recommendations</h2>
@@ -52,7 +53,7 @@ const Testimonials = () => {
             >
               <div className="testimonial__header">
                 <div className="testimonial__icon">
-                  <img src={Icon} alt="" />
+                <img src={Icon} alt="" />
                 </div>
 
                 <img src={img} alt="" className="testimonial__img" />
